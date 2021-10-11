3bf013773b916eecc37e03daf6989bca55ec2dd0
J1yun <cjy5025@naver.com>
2021-10-11 19:19:45 +0900

    Modify Product

--- src/routes/consumer/product.controller.js
+++ src/routes/consumer/product.controller.js
@@ -1,10 +1,19 @@
 'use strict';
 
 const Product = require('../../models/consumer/Product/Product');
+const ProductStorage = require('../../models/consumer/Product/ProductStorage');
 
 const output = {
   productDetail: async (req, res) => {
-    res.render('consumer/productDetail');
+    try {
+      if (!req.params.storeId) return res.render('common/500error', { storeId: false });
+      if (!req.params.productId) return res.render('common/500error', { productId: false });
+      const productDetailInfo = await ProductStorage.getProductDetailInfo(req.params.storeId, req.params.productId);
+      const productOptionInfo = await ProductStorage.getProductOptionInfoByProductId(req.params.productId);
+      res.render('consumer/productDetail', { productDetailInfo, productOptionInfo });
+    } catch (err) {
+      res.render('common/500error', { err, layout: false });
+    }
   },
 };
 
