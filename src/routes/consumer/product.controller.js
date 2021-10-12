41073b2d22a4e220e5af50cbae8e82fd1fa4be7a
J1yun <cjy5025@naver.com>
2021-10-13 01:42:04 +0900

    Product API connect

--- src/routes/consumer/product.controller.js
+++ src/routes/consumer/product.controller.js
@@ -8,9 +8,14 @@ const output = {
     try {
       if (!req.params.storeId) return res.render('common/500error', { storeId: false });
       if (!req.params.productId) return res.render('common/500error', { productId: false });
-      const productDetailInfo = await ProductStorage.getProductDetailInfo(req.params.storeId, req.params.productId);
-      const productOptionInfo = await ProductStorage.getProductOptionInfoByProductId(req.params.productId);
-      res.render('consumer/productDetail', { productDetailInfo, productOptionInfo });
+      const product = await ProductStorage.getProductDetail(req.params.storeId, req.params.productId); //TODO: 하나로 묶기, 하루최대 주문량 출력
+      const option = await ProductStorage.getProductOptionByProductId(req.params.productId);
+      var productOption = {};
+      for (var op of option) {
+        productOption[op.categoryId] ? productOption[op.categoryId].push(op) : (productOption[op.categoryId] = [op]);
+      }
+      const productImage = product.detailImageUrl ? product.detailImageUrl.split(',') : [];
+      res.render('consumer/productDetail', { product, productOption, productImage });
     } catch (err) {
       res.render('common/500error', { err, layout: false });
     }
