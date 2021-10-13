b67c75d0101a3eaee414c7861ea068b1cd3db400
이성준 <75744297+castlejun-2@users.noreply.github.com>
2021-10-13 22:36:27 +0900

    Modify: AWS S3 Image Upload API

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,13 +1,16 @@
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const aws = require('aws-sdk');
-aws.config.loadFromPath(__dirname + '/awsconfig.json');
-const s3 = new aws.S3();
+const s3 = new aws.S3({
+  accessKeyId: process.env.S3_ACCESS_KEY,
+  secretAccessKey: process.env.S3_PRIVATE_KEY,
+  region: process.env.REGION,
+});
 
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: 'img.castlejun-2.shop',
+    bucket: process.env.BUCKET_NAME,
     contentType: multerS3.AUTO_CONTENT_TYPE,
     acl: 'public-read',
     key: (req, file, cb) => {
--- src/routes/seller/index.js
+++ src/routes/seller/index.js
@@ -16,9 +16,9 @@ router.get('/licenseNum', storeController.output.licenseNum); //사업자 등록
 router.post('/register', hostController.process.register); //회원가입
 router.post('/login', hostController.process.login); //스토어 로그인
 router.post('/logout', hostController.process.logout); //스토어 로그아웃
-router.post('/licenseNum', storeController.process.licenseNum); //사업자 등록번호 조회
+router.post('/register/licenseNum', storeController.process.licenseNum); //사업자 등록번호 조회
+router.post('/register/uploadImage', upload.single('uploadImage'), hostController.process.uploadImage); //이미지 업로드
 router.post('/store/storePage', storeController.process.storePage); //스토어 상세정보 수정
-router.post('/uploadImage', upload.single('uploadImage'), hostController.process.uploadImage); //이미지 업로드
 //Order Info Router
 router.patch('/order-status', orderController.process.orderStatus); //주문 상황 변경(승인or거절→주문확정→제작완료→픽업대기→픽업완료)
 router.get('/total-order', orderController.output.order); //주문 내역 리스트 조회 → pagenation
