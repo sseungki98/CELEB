9b95bbaf9b1a3d3b6f25542827ec502a60fefc29
J1yun <cjy5025@naver.com>
2021-10-19 13:32:23 +0900

    Update index.js

--- src/routes/consumer/index.js
+++ src/routes/consumer/index.js
@@ -23,7 +23,7 @@ router.get('/mypage', userController.output.myPage); //마이페이지 조회
 router.get('/store/:storeId', storeController.output.storeDetail); //각 스토어별 페이지 조회
 router.get('/category/:categoryId', storeController.output.storeList); //카테고리별 스토어 리스트 조회
 router.get('/store/reservation/:storeId', storeController.output.storeReservationDate); //스토어 예약 불가능일 조회
-router.get('/store', storeController.output.searchStore); //스토어 통합 검색
+router.get('/search', storeController.output.searchStore); //스토어 통합 검색
 //Inquiry Info Router
 router.post('/inquiry/:storeId/:productId', inquiryController.process.inquiry); //문의 생성
 router.get('/inquiry/:storeId', inquiryController.output.inquiryDetail); //스토어별 문의 내용 조회 → 수신/발신 체크
@@ -36,7 +36,7 @@ router.get('/order/:orderId', orderController.output.order); //주문 확인 조
 //Product Info Router
 router.get('/store/:storeId/product/:productId', productController.output.productDetail); // 상품 디테일 조회
 //Review Info Router
-router.get('/mypage/order/:orderId/review', reviewController.output.review); // 리뷰 조회 페이지
+router.get('/mypage/order/:orderId/review', reviewController.output.review); // 리뷰 작성 페이지 가져오기
 router.post('/mypage/order/:orderId/review', reviewController.process.review); //스토어 리뷰 작성하기
 router.patch('/review/:reviewId', reviewController.process.patchReview); //스토어 리뷰 삭제하기
 router.get('/store/:storeId/review', reviewController.output.storeReview); //스토어 리뷰 조회
