ca0a470541fb12ab4b7607a1f2ddcbca2ce6f955
이성준 <75744297+castlejun-2@users.noreply.github.com>
2021-11-03 18:21:19 +0900

    Update seller host validation

--- src/routes/seller/host.controller.js
+++ src/routes/seller/host.controller.js
@@ -63,42 +63,11 @@ const process = {
     return res.json(response);
   },
   licenseNum: async (req, res) => {
-    // 성준: validation 수정
-    try {
-      const reg_no = req.body.licenseNum;
-      const rgno = reg_no.replace(/-/g, '');
-      const url = 'https://teht.hometax.go.kr/wqAction.do?actionId=ATTABZAA001R08&screenId=UTEABAAA13&popupYn=false&realScreenId=';
-      const body =
-        '<map id="ATTABZAA001R08"><pubcUserNo/><mobYn>N</mobYn><inqrTrgtClCd>1</inqrTrgtClCd><txprDscmNo>' +
-        rgno +
-        '</txprDscmNo><dongCode>35</dongCode><psbSearch>Y</psbSearch><map id="userReqInfoVO"/></map>';
-      const response = await axios.post(url, body, {
-        headers: {
-          'Content-Type': 'application/xml; charset=UTF-8',
-        },
-      });
-      const etree = emt.XML(response.data);
-      const ms = etree.findtext('smpcBmanTrtCntn');
-      if (ms == '등록되어 있지 않은 사업자등록번호 입니다. ')
-        return res.json({ success: false, message: '국세청에 등록되지 않은 사업자등록번호입니다.' });
-      const rgnoResult = await HostStorage.getLicenseNumberDuplication(rgno);
+    const host = new Host(req.body);
+    const response = await host.checkLicenseNumber();
 
-      if (rgnoResult.exist) return res.json({ success: false, message: '이미 등록된 사업자등록번호입니다.' });
-      return res.json({ licenseNum: rgno, message: '사용 가능한 사업자등록번호입니다.' });
-    } catch (err) {
-      return res.json({ success: false, message: err });
-    }
+    return response;
   },
-  /*uploadImage: async (req, res) => {
-    try {
-      const uploadImageName = req.file.originalname;
-      const s3ImageLocation = req.file.location;
-      return res.json({ uploadImage: uploadImageName, s3ImageUrl: s3ImageLocation });
-    } catch (err) {
-      return res.json({ success: false, ErrorCode: err, message: '사진 업로드에 실패하였습니다.' });
-    }
-  },
-  */
 };
 
 module.exports = {
