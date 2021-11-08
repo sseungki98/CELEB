19f1619319f90d6ea751392999fd9e15b4b7d768
J1yun <cjy5025@naver.com>
2021-11-08 22:44:42 +0900

    Update seller login

---
+++ src/public/js/seller/login.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const email = document.getElementById('email'),
+  password = document.getElementById('password'),
+  loginButton = document.getElementById('loginButton');
+loginButton.addEventListener('click', login);
+
+function login() {
+  const req = {
+    email: email.value,
+    password: password.value,
+  };
+  fetch('/s/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(req),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.success) {
+        location.href = '/s';
+      } else {
+        alert(res.message);
+      }
+    });
+}
