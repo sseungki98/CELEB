97f912fb386dfa9c86b2effe0765300f7c7e83dd
이성준 <75744297+castlejun-2@users.noreply.github.com>
2021-11-12 19:55:28 +0900

    Update Store Inquiry List

--- src/models/seller/Inquiry/InquiryStorage.js
+++ src/models/seller/Inquiry/InquiryStorage.js
@@ -7,19 +7,17 @@ class InquiryStorage {
     // 승환: 유저 네임 done
     //TODO: 이미지 보류, userId, userName, inquiryId, inquriyContent, createdAt
     return new Promise((resolve, reject) => {
-      const query = `select a.storeId as storeId
-      , a.storeName as storeName
-      , a.imageUrl as storeImage
-      , a.userId as userId
-      , b.name as userName
-      , a.id as inquiryId
-      , a.contents as lastContents
-      , date_format(a.createdAt, '%Y-%m-%d %H:%i') as createdAt
-from ( select i.storeId, s.storeName, s.imageUrl, i.userId, i.id, i.contents, i.createdAt, ROW_NUMBER() over (PARTITION BY i.storeId ORDER BY i.createdAt DESC) as rowNum from Inquiry i left join Store s on i.storeId=s.id where i.storeId = ?) a
-left join ( select id, name
-            from User ) as b
-            on a.userId = b.id
-where rowNum = 1;`;
+      const query = `
+        select *
+        from (
+		        select i.storeId, s.storeName, s.imageUrl, i.userId, i.id, i.contents, i.createdAt
+		        from Inquiry i left join Store s on s.id=i.storeId
+            where (i.userId,i.createdAt) in (
+				          select userId, max(createdAt) as createdAt
+				          from Inquiry group by storeId,userId) and i.storeId=?
+		        order by i.createdAt DESC) t
+        Group by t.userId;
+      `;
       db.query(query, [storeId], (err, data) => {
         if (err) reject(`${err}`);
         resolve(data);
